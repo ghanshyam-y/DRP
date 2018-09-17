eaf5b3142114906a12a934c04810d7c37e441cd4
Ghanshyam.y <ghanshyam.y@media.net>
2018-09-17 23:12:01 +0530

    adding the app and updating minor changes to model_classifier

--- data/process_data.py
+++ data/process_data.py
@@ -4,15 +4,32 @@ from sqlalchemy import create_engine
 
 
 def load_data(categories_path, messages_path):
+    """
+    Loads the categories and messages data
+    :param categories_path: String
+    :param messages_path: String
+    :return: DataFrame, DataFrame
+    """
     categories = pd.read_csv(categories_path)
     messages = pd.read_csv(messages_path)
     return categories, messages
 
 def merge_data(categories, messages):
+    """
+    Merges the categories and messages data into one dataFrame
+    :param categories: DataFrame
+    :param messages: DataFrame
+    :return: DataFrame
+    """
     merged_data = messages.merge(categories, how='outer', on='id')
     return merged_data
 
 def clean_data(data):
+    """
+    Cleans the data:
+    :param data: DataFrame
+    :return: DataFrame
+    """
     categories = data['categories'].str.split(';', expand=True)
     # select the first row of the categories dataframe
     row = categories.iloc[0]
@@ -30,19 +47,43 @@ def clean_data(data):
     data = pd.concat([data, categories], axis=1)
     # drop duplicates
     data.drop_duplicates(inplace=True)
+    data.fillna(0)
     return data
 
 
-def save_data(data):
-    engine = create_engine('sqlite:///mydata.db')
-    data.to_sql('mydata', engine, index=False)
+def save_data(data, database_filepath):
+    """
+    Save data to a sqlite database
+    :param data: DataFrame
+    :param database_filepath: String
+    :return:
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    data.to_sql('mydata1', engine, index=False)
 
-def run_pipeline(categories_path, messages_path, output_path):
+def run_pipeline(categories_path, messages_path, database_filepath):
+    """
+    Runs the ETL pipeline
+    :param categories_path:
+    :param messages_path:
+    :param database_filepath:
+    :return:
+    """
     categories, messages = load_data(categories_path, messages_path)
     data = merge_data(categories, messages)
     data = clean_data(data)
-    save_data(data)
+    save_data(data, database_filepath)
 
 if __name__ == '__main__':
-    categories_path, messages_path = sys.argv[1:]
-    run_pipeline(categories_path, messages_path)
+
+    if len(sys.argv) == 4:
+        categories_path, messages_path, database_filepath = sys.argv[1:]
+        run_pipeline(categories_path, messages_path, database_filepath)
+
+    else:
+        print('Please provide the filepaths of the messages and categories ' \
+              'datasets as the first and second argument respectively, as ' \
+              'well as the filepath of the database to save the cleaned data ' \
+              'to as the third argument. \n\nExample: python process_data.py ' \
+              'disaster_messages.csv disaster_categories.csv ' \
+              'DisasterResponse.db')
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -5,7 +5,7 @@ import pickle
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from sqlalchemy import create_engine
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -15,21 +15,29 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 
 
-
-
 def load_data(file_path):
-    engine = create_engine(file_path)
-    df = pd.read_sql_table('mydata', engine)
-    X, y = df['message'], df.iloc[:, 4:]
+    """
+    Loads data from the given sqlite data path
+    :param file_path: String
+    :return data: DataFrame
+    """
+    print('Loading data...\n    PATH: {}\n'.format(file_path))
+    engine = create_engine('sqlite:///{}'.format(file_path))
+    df = pd.read_sql_table('mydata1', engine)
+    X = df['message'].values
+    y = df.drop(['id','message','original','genre'], axis=1).values
     categories = df.columns[4:]
     return X, y, list(categories)
 
 def tokenize(text):
+    """
+        Tokenizes the text provided
+        :param text: String
+        :return: List
+    """
     text = re.sub(r"[^A-Za-z]", " ", text)
-
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
-
     clean_tokens = []
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
@@ -38,43 +46,76 @@ def tokenize(text):
     return clean_tokens
 
 def build_model(tokenizer):
+    """
+    Builds a model pipeline
+    :param tokenizer: function
+    :return: model
+    """
+    print('Building Model')
     pipeline = Pipeline([('vect', CountVectorizer(tokenizer = tokenizer)),
                          ('tfidf', TfidfTransformer()),
                          ('clf', MultiOutputClassifier(RandomForestClassifier()))])
     # Parameters to grid search
     parameters = {'tfidf__use_idf': (True, False),
-                               'vect__ngram_range': [(1, 1), (1, 2)],
-                               'tfidf__smooth_idf': (True, False)}
+                  'vect__ngram_range': [(1, 1), (1, 2)],
+                  'tfidf__smooth_idf': (True, False)}
+
     # Grid search
-    model_pipeline = GridSearchCV(pipeline, parameters, n_jobs=-1)
+    model_pipeline = GridSearchCV(pipeline, parameters)
     return model_pipeline
 
 def train(X, y, categories, model):
-
+    """
+    Trains a given model
+    :param X: Data
+    :param y: labels
+    :param categories: label_names
+    :param model: model
+    :return: model
+    """
     # train test split
     X_train, X_test, y_train, y_test = train_test_split(X, y)
-
     # Train the model
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     # Print results
-    print(classification_report(y_test, y_pred, target_names=categories))
+    for index, category in enumerate(categories):
+        print(classification_report(y_test[:, index], y_pred[:, index], target_names=[category]))
     return model
 
 def export_model(model, model_filepath):
+    """
+    Save the model to a pickle file
+    :param model: model
+    :param model_filepath: String
+    :return:
+    """
     # Save the model
     pickle.dump(model, open(model_filepath, "wb"))
 
 
 def run_pipeline(data_file, model_filepath):
+    """
+    Runs the ML pipeline
+    :param data_file: String
+    :param model_filepath: String
+    :return:
+    """
     X, y, categories = load_data(data_file)  # run ETL pipeline
     model = build_model(tokenizer=tokenize)  # build model pipeline
     model = train(X, y, categories, model)  # train model pipeline
     export_model(model, model_filepath)  # save model
 
 if __name__ == '__main__':
-    data_file, model_filepath = sys.argv[1:]
-    run_pipeline(data_file=data_file, model_filepath=model_filepath)
 
+    if len(sys.argv) == 3:
+        data_file, model_filepath = sys.argv[1:]
+        run_pipeline(data_file=data_file, model_filepath=model_filepath)
+
+    else:
+        print('Please provide the filepath of the disaster messages database ' \
+              'as the first argument and the filepath of the pickle file to ' \
+              'save the model to as the second argument. \n\nExample: python ' \
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
